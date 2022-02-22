24160b5229610b566da01855af63a5bb6c27683d
soulteary <soulteary@gmail.com>
2022-02-22 23:05:30 +0800

    chore: Data module: serializer

---
+++ data/serializer.go
@@ -0,0 +1,47 @@
+package data
+
+import (
+	"bytes"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"io"
+	"strings"
+	"time"
+)
+
+func jsonStringify(data interface{}) string {
+	buff := bytes.NewBuffer([]byte{})
+	encoder := json.NewEncoder(buff)
+	encoder.SetEscapeHTML(false)
+	encoder.Encode(data)
+	return strings.TrimSpace(buff.String())
+}
+
+func MaskTextWithStars(input string) string {
+	chars := []rune(input)
+	mask := chars
+	count := len(chars)
+	for i := 0; i < count; i++ {
+		if i != 0 && i != count-1 {
+			mask[i] = '*'
+		}
+	}
+	return string(mask)
+}
+
+func GenerateRandomString(size int) string {
+	id := make([]byte, 32)
+	if _, err := io.ReadFull(rand.Reader, id); err != nil {
+		data := []byte(time.Now().String())
+		hash := md5.Sum(data)
+		return hex.EncodeToString(hash[0:size])
+	}
+	return hex.EncodeToString(id)[:size]
+}
+
+func Base64Encode(input string) string {
+	return base64.StdEncoding.EncodeToString([]byte(input))
+}
---
+++ data/serializer_test.go
@@ -0,0 +1,46 @@
+package data
+
+import (
+	"testing"
+)
+
+func TestJSONStringify(t *testing.T) {
+	type testJSON struct {
+		Project string `json:"project"`
+	}
+	const src = "{\"project\":\"flare\"}"
+	dest := jsonStringify(testJSON{Project: "flare"})
+
+	if src != dest {
+		t.Fatal("JSON Stringify Error")
+	}
+}
+
+func TestMaskTextWithStars(t *testing.T) {
+	if MaskTextWithStars("1234") != "1**4" {
+		t.Fatal("MaskTextWithStars Error")
+	}
+
+	if MaskTextWithStars("123") != "1*3" {
+		t.Fatal("MaskTextWithStars Error")
+	}
+
+	if MaskTextWithStars("12") != "12" {
+		t.Fatal("MaskTextWithStars Error")
+	}
+
+	if MaskTextWithStars("1") != "1" {
+		t.Fatal("MaskTextWithStars Error")
+	}
+
+	if MaskTextWithStars("") != "" {
+		t.Fatal("MaskTextWithStars Error")
+	}
+}
+
+func TestGenerateRandomString(t *testing.T) {
+	name1 := GenerateRandomString(8)
+	if len(name1) != 8 {
+		t.Fatal("GenerateRandomString length error")
+	}
+}
