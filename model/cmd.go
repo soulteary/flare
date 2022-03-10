b8b21ab88c44d8c1b5288274a5ea40fa7fe471c3
soulteary <soulteary@gmail.com>
2022-03-10 14:36:27 +0800

    feat: add envfile support

--- model/cmd.go
+++ model/cmd.go
@@ -35,3 +35,18 @@ type Envs struct {
 	User             string `env:"FLARE_USER,unset"`
 	Pass             string `env:"FLARE_PASS,unset"`
 }
+
+// Application Envfile Data Model
+type EnvFile struct {
+	Port                   int  `ini:"FLARE_PORT,omitempty"`
+	EnableGuide            bool `ini:"FLARE_GUIDE,omitempty"`
+	EnableEditor           bool `ini:"FLARE_EDITOR,omitempty"`
+	EnableOfflineMode      bool `ini:"FLARE_OFFLINE,omitempty"`
+	EnableMinimumRequest   bool `ini:"FLARE_MINI_REQUEST,omitempty"`
+	EnableDeprecatedNotice bool `ini:"FLARE_DEPRECATED_NOTICE,omitempty"`
+
+	Visibility       string `ini:"FLARE_VISIBILITY,omitempty"`
+	DisableLoginMode bool   `ini:"FLARE_DISABLE_LOGIN,omitempty"`
+	User             string `ini:"FLARE_USER,omitempty"`
+	Pass             string `ini:"FLARE_PASS,omitempty"`
+}
